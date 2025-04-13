d14ad53654fb2fe28a00e0d52e7a561175d83097
kirillun1027 <kirillun1027@gmail.com>
2025-04-13 16:51:22 +0500

    optimized stuff

--- emotionrecognition/trigger.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-@onready var jumpscare = $"../jumpscare"
-var battle := preload("res://battle.tscn")
-
-func _on_body_entered(body: CharacterBody2D) -> void:
-	get_tree().change_scene_to_file("res://battle.tscn")
